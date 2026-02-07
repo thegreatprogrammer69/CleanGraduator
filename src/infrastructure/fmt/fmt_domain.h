2f99bb1e0ff89d60d783df4f7b79354b941eef73
thegreatprogrammer69 <thegreatprogrammer69@gmail.com>
2026-02-07 20:46:27 +0700

    Add stream formatters for domain types

--- src/infrastructure/fmt/fmt_domain.h
+++ src/infrastructure/fmt/fmt_domain.h
@@ -4,13 +4,27 @@
 
 namespace domain::common {
     class Angle;
-    class AngleUnit;
+    enum class AngleUnit;
+    class Pressure;
+    enum class PressureUnit;
+    class Timestamp;
+    enum class PixelFormat;
+    struct VideoBuffer;
+    struct VideoFrame;
+    enum class ProcessLifecycleState;
 }
 
 namespace fmt {
     using namespace domain::common;
     std::ostream& operator<<(std::ostream& os, const Angle& f); // Выводит значение вместе с его текущим Unit
     std::ostream& operator<<(std::ostream& os, const AngleUnit& f);
+    std::ostream& operator<<(std::ostream& os, const Pressure& f);
+    std::ostream& operator<<(std::ostream& os, const PressureUnit& f);
+    std::ostream& operator<<(std::ostream& os, const Timestamp& f);
+    std::ostream& operator<<(std::ostream& os, const PixelFormat& f);
+    std::ostream& operator<<(std::ostream& os, const VideoBuffer& f);
+    std::ostream& operator<<(std::ostream& os, const VideoFrame& f);
+    std::ostream& operator<<(std::ostream& os, const ProcessLifecycleState& f);
 }
 
 #endif //CLEANGRADUATOR_FMT_DOMAIN_H
